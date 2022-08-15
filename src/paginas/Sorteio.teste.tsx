dfcfbea132036d4a7ccc0f2f090443dc6c3c7559
Raphael Fonseca <pedro@clubedodev.com.br>
2022-08-15 15:17:50 -0300

    4.2 Pagina de sorteio

---
+++ src/paginas/Sorteio.teste.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { RecoilRoot } from 'recoil';
+import { useListaDeParticipantes } from '../state/hook/useListaDeParticipantes';
+import Sorteio from './Sorteio';
+
+jest.mock('../state/hook/useListaDeParticipantes', () => {
+  return  {
+    useListaDeParticipantes: jest.fn()
+  }
+})
+
+describe('na pagina de sorteio', () => {
+  test('todos os participantes podem exibir o seu amigo secreto', () => {
+    const participantes = [ 
+      'Raphael',
+      'Dianna',
+      "Felipe"
+    ]
+
+    beforeEach(() => {
+      (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
+    })
+
+    render(<RecoilRoot>
+        <Sorteio />
+      </RecoilRoot>)
+
+      const opcoes = screen.queryAllByRole("option")
+      expect(opcoes).toHaveLength(participantes.length)
+  })
+})
