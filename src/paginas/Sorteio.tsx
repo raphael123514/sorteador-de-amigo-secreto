8af1434ebef86bf7f19055507ccbb863ce308981
Raphael Fonseca <pedro@clubedodev.com.br>
2022-08-17 02:34:39 -0300

    4.7 Desafio: ultimos estilos de configração

--- src/paginas/Sorteio.tsx
+++ src/paginas/Sorteio.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react"
+import Cabecalho from "../componentes/Cabecalho"
+import Card from "../componentes/Card"
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes"
 import { useResultadoDoSorteio } from "../state/hook/useResultadoDoSorteio"
+import './Sorteio.css'
 
 const Sorteio = () => {
 
@@ -20,21 +23,32 @@ const Sorteio = () => {
   }
 
   return (
-    <section>
-      <form onSubmit={sortear}>
-        <select 
-          required 
-          name="participanteDaVez" 
-          id="participanteDaVez" 
-          placeholder="Selecione o seu nome"
-          value={participanteDaVez}
-          onChange={evento => setParticipanteDaVez(evento.target.value)}>
-          {participantes.map(participante => <option key={participante}>{participante}</option>)}
-        </select>
-        <button>Sortear</button>
-      </form>
-      {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
-    </section>
+    <>
+      <Cabecalho />
+      <Card>
+        <section className="sorteio">
+          <h2>Quem vai tirar o papelzinho?</h2>
+          <form onSubmit={sortear}>
+            <select 
+              required 
+              name="participanteDaVez" 
+              id="participanteDaVez" 
+              placeholder="Selecione o seu nome"
+              value={participanteDaVez}
+              onChange={evento => setParticipanteDaVez(evento.target.value)}>
+              {participantes.map(participante => <option key={participante}>{participante}</option>)}
+            </select>
+            <p>Clique em em sortear para ver quem é seu amigo secreto!</p>
+            <button className="botao-sortear">Sortear</button>
+          </form>
+          {amigoSecreto && <p role="alert">{amigoSecreto}</p>}
+          <footer className="sorteio">
+            <img src="/imagens/aviao.png" className="aviao" alt="Um desenho de um avião de papel" />
+          </footer>
+
+        </section>
+      </Card>
+    </>
   )
 }
 
