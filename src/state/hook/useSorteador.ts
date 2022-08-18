6f79d1cae35b23e5be3159986acf67c26b49bbd8
Raphael Fonseca <pedro@clubedodev.com.br>
2022-08-18 00:23:39 -0300

    correção

--- src/state/hook/useSorteador.ts
+++ src/state/hook/useSorteador.ts
@@ -1,4 +1,3 @@
-import shuffle from "just-shuffle"
 import { useSetRecoilState } from "recoil"
 import { resultadoDoAmigoSecreto } from "../atom"
 import { realizarSorteio } from "../helpers/realizarSorteio"
